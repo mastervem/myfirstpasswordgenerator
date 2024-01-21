665efff85cd206f466f5b462ff10265eca97a3da
mastervem <velezanthony3535@gmail.com>
2024-01-21 13:16:32 -0600

    just add var for lower case and special characters

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "0123456789";
+let uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let temporaryPassword = [];
 
+console.log('temporaryPassword ', temporaryPassword);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +13,26 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+
+function generatePassword(){
+  let passLength = prompt("Please choose length of password within 8 and 128");
+
+  console.log('passLength ',passLength)
+
+
+  let lowerCaseQuestion = confirm("Would you like lowercases?")
+
+  if(lowerCaseQuestion){
+    temporaryPassword.push(lowercase)
+  }
+
+  console.log('temporaryPassword after push  ', temporaryPassword)
+
+
+  
+  // return final data 
 }
 
 // Add event listener to generate button
