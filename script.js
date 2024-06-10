3a4c16b23d5944f4cd9a7d4491e6d174b957b935
mastervem <velezanthony3535@gmail.com>
2024-06-09 20:02:09 -0500

    i created a readme file

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 let lowercase = "abcdefghijklmnopqrstuvwxyz";
 let numbers = "0123456789";
 let uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let temporaryPassword = [];
+let specialCharacters = "!@#$%&*?~:";
+let temporaryPassword = "";
 
 console.log('temporaryPassword ', temporaryPassword);
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -17,23 +18,38 @@ function writePassword() {
 
 
 function generatePassword(){
+  temporaryPassword =""
   let passLength = prompt("Please choose length of password within 8 and 128");
-
+let password =""
   console.log('passLength ',passLength)
 
 
   let lowerCaseQuestion = confirm("Would you like lowercases?")
 
   if(lowerCaseQuestion){
-    temporaryPassword.push(lowercase)
+   
+    temporaryPassword = temporaryPassword + lowercase
   }
-
-  console.log('temporaryPassword after push  ', temporaryPassword)
-
-
+let specialCharactersQuestion = confirm("Would you like special characters?")
+if(specialCharacters){
   
-  // return final data 
+  temporaryPassword = temporaryPassword + specialCharacters
+}
+let uppercaseQuestion = confirm("would you like uppercase")
+if(uppercase){
+ 
+  temporaryPassword = temporaryPassword + uppercase
+} 
+console.log('temporaryPassword after push  ', temporaryPassword)
+console.log
+for (let index = 0; index <passLength; index++) {
+  password += temporaryPassword[Math.floor(Math.random() * temporaryPassword.length)];
+
+}
+return password;
+  
+   
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
